fd218a735a12b15bd38977df574347e390dd8d60
Martin Flischman <martincodes@icloud.com>
2024-09-02 15:41:04 +0200

    Layout update
    chore: spacer added to move the input and buttons to the bottom of the screen
    
    chore: another spacer added to add some space above the output text
    
    chore: padding added to the output text's vstack so that it doesn't go to the edge of the screen

--- Texty/ContentView.swift
+++ Texty/ContentView.swift
@@ -13,48 +13,53 @@ struct ContentView: View {
     @State var buttonPressed = 0
     
     var body: some View {
-        VStack {
-
-            if buttonPressed == 1 {
-                Text(userText.uppercased())
-                    .font(.largeTitle)
-                    .padding(.bottom, 100)
-            } else if buttonPressed == 2 {
-                Text(userText.capitalized)
-                    .font(.largeTitle)
-                    .padding(.bottom, 100)
-            } else {
-                Text(userText.lowercased())
-                    .font(.largeTitle)
-                    .padding(.bottom, 100)
-            }
-            
-            TextField("Type here...", text: $userText)
-                .font(.title3)
-                .foregroundColor(.secondary)
-                .padding(.bottom, 20)
-
-            HStack {
-                
-                Button(action: {
-                    buttonPressed = 1
-                }) {
-                    CustomButtonView(title: "ALL CAPS", color: .orange)
-                }
-                
-                Button(action: {
-                    buttonPressed = 2
-                }) {
-                    CustomButtonView(title: "1st Letter", color: .blue)
-                }
-                
-                Button(action: {
-                    buttonPressed = 3
-                }) {
-                    CustomButtonView(title: "lowercase", color: .green)
+        Spacer()
+        
+            VStack {
+                if buttonPressed == 1 {
+                    Text(userText.uppercased())
+                        .font(.largeTitle)
+                        .padding(.bottom, 100)
+                } else if buttonPressed == 2 {
+                    Text(userText.capitalized)
+                        .font(.largeTitle)
+                        .padding(.bottom, 100)
+                } else {
+                    Text(userText.lowercased())
+                        .font(.largeTitle)
+                        .padding(.bottom, 100)
                 }
             }
+            .padding()
             
+        Spacer()
+        
+            VStack {
+                TextField("Type here...", text: $userText)
+                    .font(.title3)
+                    .foregroundColor(.secondary)
+                    .padding(.bottom, 20)
+
+                HStack {
+                    
+                    Button(action: {
+                        buttonPressed = 1
+                    }) {
+                        CustomButtonView(title: "ALL CAPS", color: .orange)
+                    }
+                    
+                    Button(action: {
+                        buttonPressed = 2
+                    }) {
+                        CustomButtonView(title: "1st Letter", color: .blue)
+                    }
+                    
+                    Button(action: {
+                        buttonPressed = 3
+                    }) {
+                        CustomButtonView(title: "lowercase", color: .green)
+                    }
+            }
         }
         .padding()
     }
